05879dc6c87a5e6b049ff411595140ee8c853ce6
benjaminpaige <benjamin.paige@gmail.com>
2018-07-01 16:45:29 -0600

    Add testing for Tail class

---
+++ test/Tail-test.js
@@ -0,0 +1,36 @@
+const { assert } = require('chai');
+const Tail = require('../lib/Tail');
+
+describe('Tail', () => {
+
+  it('should instantiate an object', () => {
+    let tail = new Tail(100, 100, 10, 10, '#ffffff');
+
+    assert.equal(typeof tail, 'object');
+  });
+
+  it('should have a starting point', () => {
+    let tail = new Tail(100, 100, 10, 10, '#ffffff');
+
+    assert.equal(tail.x, 100);
+    assert.equal(tail.y, 100);
+  })
+
+  it('should have a width', () => {
+    let tail = new Tail(100, 100, 10, 10, '#ffffff');
+
+    assert.equal(tail.width, 10);
+  })
+
+  it('should have a height', () => {
+    let tail = new Tail(100, 100, 10, 10, '#ffffff');
+
+    assert.equal(tail.height, 10);
+  })
+
+  it('should have a color', () => {
+    let tail = new Tail(100, 100, 10, 10, '#ffffff');
+
+    assert.equal(tail.color, "#ffffff");
+  })
+})
