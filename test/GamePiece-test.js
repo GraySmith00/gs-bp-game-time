1079236b89cb4c00c778ab1ee5229d1dc4a8a97f
Gray Smith <gray.smith00@gmail.com>
2018-07-02 23:18:45 -0600

    Add draw test to GamePiece test

--- test/GamePiece-test.js
+++ test/GamePiece-test.js
@@ -2,6 +2,20 @@ const { assert } = require('chai');
 const GamePiece = require('../lib/GamePiece');
 
 describe('GamePiece', function() {
+  class Context {
+    constructor() {
+      this.canvas = 'canvas#game';
+      this.fillStyle = '#000000';
+      this.filter = 'none';
+      this.font = '10px sans-serif';
+      this.globalAlpha = 1;
+    }
+
+    fillRect(x, y, width, height) {
+      return `${(x, y, width, height)}`;
+    }
+  }
+
   it('should have properties', function() {
     const gamePiece = new GamePiece(50, 75, 10, 10, 'rgba(0, 255, 0, 1)');
 
@@ -30,5 +44,13 @@ describe('GamePiece', function() {
     const isColliding = gamePiece1.isCollidingWith(gamePiece2);
 
     assert.isFalse(isColliding);
+  });
+
+  it('should draw to the canvas', function() {
+    const ctx = new Context();
+    const gamePiece = new GamePiece(50, 75, 10, 10, 'rgba(0, 255, 0, 1)');
+
+    gamePiece.draw(ctx);
+    assert.equal(ctx.fillStyle, 'rgba(0, 255, 0, 1)');
   });
 });
